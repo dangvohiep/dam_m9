ad03ca69802c007ee9a1c5d2a10a60b685746896
dangvohiep <hiepdangvo@icloud.com>
2023-11-08 17:18:13 -0500

    change json

--- task1.py
+++ task1.py
@@ -1,45 +1,51 @@
 import json
 import pandas as pd
 
-source = {
-    "albums": 
-    [
-        {
-            "title"     : "Dangerous: The Double Album",
-            "artist"    : "Morgan Wallen",
-            "songs"     : [
-                "Sand in My Boots",
-                "Wasted on You",
-                "Somebody's Problem",
-                "More Surprised than Me",
-                "865",
-            ],
-        },
-        {
-            "title"     : "The Mockingbird & the Crow",
-            "artist"    : "Hardy",
-            "songs"     : [
-                "Beer",
-                "Red",
-                "Wait in the Truck",
-                "Drink One for Me",
-                "I in Country",
-            ],
-        },
-        {
-            "title"     : "Speak Now",
-            "artist"    : "Taylor Swift",
-            "songs"     : [
-                "Mine",
-                "Sparks Fly",
-                "Back to December",
-                "Speak Now",
-                "Dear John",
-            ],
-        }
-    ]
-}
+albums = [
+    {
+        "title"     : "Dangerous: The Double Album",
+        "artist"    : "Morgan Wallen",
+        "songs"     : [
+            "Sand in My Boots",
+            "Wasted on You",
+            "Somebody's Problem",
+            "More Surprised than Me",
+            "865",
+        ],
+    },
+    {
+        "title"     : "The Mockingbird & the Crow",
+        "artist"    : "Hardy",
+        "songs"     : [
+            "Beer",
+            "Red",
+            "Wait in the Truck",
+            "Drink One for Me",
+            "I in Country",
+        ],
+    },
+    {
+        "title"     : "Speak Now",
+        "artist"    : "Taylor Swift",
+        "songs"     : [
+            "Mine",
+            "Sparks Fly",
+            "Back to December",
+            "Speak Now",
+            "Dear John",
+        ],
+    }
+]
 
-# print(json.dumps(source, indent=2))
-print(pd.DataFrame(data=source["albums"]).to_html(index=False))
+
+# print(json.dumps(albums, indent=2))
+# print(pd.DataFrame(albums).to_html(index=False))
+
+# table_from_json = pd.read_json(
+#     "https://raw.githubusercontent.com/dangvohiep/dam_m9/master/albums.json"
+# )
+# table_from_html = pd.read_html(
+#     "https://raw.githubusercontent.com/dangvohiep/dam_m9/master/albums.html"
+# )
+
 
